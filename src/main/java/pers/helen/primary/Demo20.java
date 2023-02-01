043c5e70a2860dc3d25b9642e71d11b6436049f1
姜敏 <jiangmin@cditv.tv>
2023-02-01 16:38:35 +0800

    有效的括号

--- src/main/java/pers/helen/primary/Demo20.java
+++ src/main/java/pers/helen/primary/Demo20.java
@@ -1,6 +1,8 @@
 package pers.helen.primary;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -17,49 +19,80 @@ public class Demo20 {
         //        String s = "[(])";
         //        String s = "()[]{}";
         //                String s = "(]";
-        //                String s = "([)]";
-        //                String s = "([[])]";
-        //        String s = "{[]}";
-        //                        String s = "({[)";
-        String s = "(([]){})";
+                        String s = "([)]";
+//                        String s = "([[])]";
+//                String s = "{[]}";
+//                                String s = "({[)";
+//        String s = "(([]){})";
         System.out.println(isValid(s));
     }
 
     public static boolean isValid(String s){
         int length = s.length();
+        // 奇数个肯定不是
         if(length % 2 != 0){
             return false;
         }
         char[] c = s.toCharArray();
-        Map<String, Integer> left = new TreeMap<>();
-        Map<Integer, Character> right = new TreeMap<>();
+
+        // 记录各个符号出现的下标
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+        List<Integer> l3 = new ArrayList<>();
+        List<Integer> r1 = new ArrayList<>();
+        List<Integer> r2 = new ArrayList<>();
+        List<Integer> r3 = new ArrayList<>();
         for(int i = 0; i < length; i++){
-            if(c[i] == '[' || c[i] == '{' || c[i] == '('){
-                left.put(c[i] + "" + i, i);
-            }else{
-                right.put(i, c[i]);
+            if(c[i] == '['){
+                l1.add(i);
+            }else if(c[i] == '{'){
+                l2.add(i);
+            }else if(c[i] == '('){
+                l3.add(i);
+            }else if(c[i] == ']'){
+                r1.add(i);
+            }else if(c[i] == '}'){
+                r2.add(i);
+            }else if(c[i] == ')'){
+                r3.add(i);
             }
         }
-        if(left.size() != right.size()){
+
+        // 同组合符号个数不匹配
+        if((l1.size() != r1.size()) || (l2.size() != r2.size()) || (l3.size() != r3.size())){
             return false;
         }
-        Map<Character, Character> rules = new HashMap<>();
-        rules.put('[', ']');
-        rules.put('{', '}');
-        rules.put('(', ')');
-        for(Map.Entry<String, Integer> map : left.entrySet()){
-            char key = map.getKey().toCharArray()[0];
-            int value = map.getValue();
-            int find = s.indexOf(rules.get(key), value);
-            if(find > value){
-                // 找到了
-                // 判断是否合法
-                if((find - value) % 2 != 0){
-                    right.remove(find);
-                }else{
-                    return false;
-                }
-            }else{
+
+        for(int i = l1.size() - 1; i >= 0; i--){
+            int l = l1.get(i);
+            int r = r1.get(l1.size() - 1 - i);
+            if(l > r){
+                return false;
+            }
+            if((r - l) % 2 == 0){
+                // 两者距离是偶数，不可能
+                return false;
+            }
+        }
+        for(int i = l2.size() - 1; i >= 0; i--){
+            int l = l2.get(i);
+            int r = r2.get(l2.size() - 1 - i);
+            if(l > r){
+                return false;
+            }
+            if((r - l) % 2 == 0){
+                // 两者距离是偶数，不可能
+                return false;
+            }
+        }
+        for(int i = l3.size() - 1; i >= 0; i--){
+            int l = l3.get(i);
+            int r = r3.get(l3.size() - 1 - i);
+            if(l > r){
+                return false;
+            }
+            if((r - l) % 2 == 0){
+                // 两者距离是偶数，不可能
                 return false;
             }
         }
