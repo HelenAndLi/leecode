c368dd97df23bb02d8fb7015b5900e3b9d7e52d8
姜敏 <jiangmin@cditv.tv>
2023-12-05 10:03:42 +0800

    （中等）：15、三数之和

--- src/main/java/pers/helen/middle/Demo15.java
+++ src/main/java/pers/helen/middle/Demo15.java
@@ -1,10 +1,10 @@
 package pers.helen.middle;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 15.三数之和（https://leetcode.cn/problems/3sum/）
@@ -28,46 +28,116 @@ import java.util.Map;
 public class Demo15 {
 
     public static void main(String[] args){
-        int[] nums = {-1, 0, 1, 2, -1, -4};
-        //        int[] nums = {};
+        //        int[] nums = {-1, 0, 1, 2, -1, -4,-1, 0, 1, 2, -1, -4};
+        //                int[] nums = {};
+        //        int[] nums = {-2, 0, 1, 1, 2};
+//                int[] nums = {-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6};
+//        int[] nums = {-1, 1, 2, 0, -1, -4};
+//                        int[] nums = {0, 0, 0, 0};
+                        int[] nums = {-1,0,1,2,-1,-4,-2,-3,3,0,4};
         System.out.println(threeSum(nums));
     }
 
     public static List<List<Integer>> threeSum(int[] nums){
         List<List<Integer>> result = new ArrayList<>();
+        // 元素个数不足，直接返回
+        if(nums.length < 3){
+            return result;
+        }
+        // 排序
+        List<Integer> list = Arrays.stream(nums).boxed().sorted().collect(Collectors.toList());
+        int length = list.size();
+        int i = 0;
+        // 只遍历到倒数第三个元素
+        while(i < length - 2){
+
+            int numi = list.get(i);
+            // 这是就是从当前下标为i元素的下一个元素开始向右遍历
+            int toRight = i + 1;
+            // 这里是从列表最后一个元素开始向左遍历
+            int toLeft = length - 1;
+            // 两个元素遍历不断靠近，但不让他们相遇甚至是擦肩而过
+            while(toLeft > toRight){
+                int numToRight = list.get(toRight);
+                int numToLeft = list.get(toLeft);
+                int sum = numToRight + numToLeft + numi;
+                // 三者之和大于0，说明最大的那个元素（也就是toLeft）太大了，要再往前走走，找个小点的
+                if(sum > 0){
+                    toLeft--;
+                }else if(sum < 0){
+                    // 三者之和小于0，说明较小的那个元素（也就是toLeft）太小了，要再往后继续走走，找个大点的
+                    toRight++;
+                }else{
+                    result.add(Arrays.asList(new Integer[]{numi, numToRight, numToLeft}));
+                    while(numToLeft == list.get(--toLeft) && toLeft > toRight){
+                        // 这里是为了去重，往左（即往前）遍历的那个下标，如果在当前轮已经用来判断过了，就没必要再走一遍流程了，要注意也要遵循不跟另一个方向来的指针相遇
+                    }
+                    while(numToRight == list.get(++toRight) && toLeft > toRight){
+                        // 同上
+                    }
+                }
+            }
+            while(i < length - 1 && numi == list.get(++i)){
+                // 这里也是为了去重，如果某一数值已经用来走过流程了，就没必要再走了，直接跳过，也要注意越界问题
+                continue;
+            }
+        }
+        return result;
+    }
+
+    public static List<List<Integer>> threeSum1(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
         if(nums.length < 2){
             return result;
         }
-        Map<Integer,Boolean> notFound = new HashMap<>();
+        List<String> found = new ArrayList<>();
         for(int i = 0; i < nums.length - 2; i++){
             for(int j = i + 1; j < nums.length - 1; j++){
                 if(i == j){
                     continue;
                 }
-                int ni = nums[i];
-                int nj = nums[j];
-                int iAndJ = ni + nj;
-                if(notFound.containsKey(iAndJ)){
-                    continue;
-                }
-                boolean found = false;
+                int iAndJ = nums[i] + nums[j];
+
                 for(int k = j + 1; k < nums.length; k++){
-                    int kk = nums[k];
-                    if(iAndJ + kk == 0){
-                        List<Integer> list = new ArrayList<>();
-                        list.add(ni);
-                        list.add(nj);
-                        list.add(kk);
-                        Collections.sort(list);
-                        if(!result.contains(list)){
-                            result.add(list);
+                    if(iAndJ + nums[k] == 0){
+                        int ni = nums[i];
+                        int nj = nums[j];
+                        int nk = nums[k];
+                        String temp;
+                        if(ni >= nj && ni >= nk){
+                            if(nj > nk){
+                                temp = ni + "," + nj + "," + nk;
+                            }else{
+                                temp = ni + "," + nk + "," + nj;
+                            }
+                        }else if(nj >= ni && nj >= nk){
+                            if(ni >= nk){
+                                temp = nj + "," + ni + "," + nk;
+                            }else{
+                                temp = nj + "," + nk + "," + ni;
+                            }
+                        }else{
+                            if(ni >= nj){
+                                temp = nk + "," + ni + "," + nj;
+                            }else{
+                                temp = nk + "," + nj + "," + ni;
+                            }
                         }
-                        found = true;
-                        break;
+
+                        if(!found.contains(temp)){
+                            List<Integer> list = new ArrayList<>();
+                            list.add(nums[i]);
+                            list.add(nums[j]);
+                            list.add(nums[k]);
+                            Collections.sort(list);
+                            result.add(list);
+                            found.add(temp);
+
+                            List<String> l = Arrays.stream(temp.split(",")).toList();
+                            l.stream().map(s->Integer.parseInt(s));
+                        }
+
                     }
-                }
-                if(!found){
-                    notFound.put(iAndJ,true);
                 }
             }
         }
--- src/main/java/pers/helen/middle/Demo8.java
+++ src/main/java/pers/helen/middle/Demo8.java
@@ -1,11 +1,14 @@
 package pers.helen.middle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 8. 字符串转换整数 (atoi)（https://leetcode.cn/problems/string-to-integer-atoi/）
  * 请你来实现一个 myAtoi(string s) 函数，使其能将字符串转换成一个 32 位有符号整数（类似 C/C++ 中的 atoi 函数）。
- *
+ * <p>
  * 函数 myAtoi(string s) 的算法如下：
- *
+ * <p>
  * 读入字符串并丢弃无用的前导空格
  * 检查下一个字符（假设还未到字符末尾）为正还是负号，读取该字符（如果有）。 确定最终结果是负数还是正数。 如果两者都不存在，则假定结果为正。
  * 读入下一个字符，直到到达下一个非数字字符或到达输入的结尾。字符串的其余部分将被忽略。
@@ -13,57 +16,215 @@ package pers.helen.middle;
  * 如果整数数超过 32 位有符号整数范围 [−231,  231 − 1] ，需要截断这个整数，使其保持在这个范围内。具体来说，小于 −231 的整数应该被固定为 −231 ，大于 231 − 1 的整数应该被固定为 231 − 1 。
  * 返回整数作为最终结果。
  * 注意：
- *
+ * <p>
  * 本题中的空白字符只包括空格字符 ' ' 。
  * 除前导空格或数字后的其余字符串外，请勿忽略 任何其他字符。
- *
- *
+ * <p>
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：s = "42"
  * 输出：42
  * 解释：加粗的字符串为已经读入的字符，插入符号是当前读取的字符。
  * 第 1 步："42"（当前没有读入字符，因为没有前导空格）
- *          ^
+ * ^
  * 第 2 步："42"（当前没有读入字符，因为这里不存在 '-' 或者 '+'）
- *          ^
+ * ^
  * 第 3 步："42"（读入 "42"）
- *            ^
+ * ^
  * 解析得到整数 42 。
  * 由于 "42" 在范围 [-231, 231 - 1] 内，最终结果为 42 。
  * 示例 2：
- *
+ * <p>
  * 输入：s = "   -42"
  * 输出：-42
  * 解释：
  * 第 1 步："   -42"（读入前导空格，但忽视掉）
- *             ^
+ * ^
  * 第 2 步："   -42"（读入 '-' 字符，所以结果应该是负数）
- *              ^
+ * ^
  * 第 3 步："   -42"（读入 "42"）
- *                ^
+ * ^
  * 解析得到整数 -42 。
  * 由于 "-42" 在范围 [-231, 231 - 1] 内，最终结果为 -42 。
  * 示例 3：
- *
+ * <p>
  * 输入：s = "4193 with words"
  * 输出：4193
  * 解释：
  * 第 1 步："4193 with words"（当前没有读入字符，因为没有前导空格）
- *          ^
+ * ^
  * 第 2 步："4193 with words"（当前没有读入字符，因为这里不存在 '-' 或者 '+'）
- *          ^
+ * ^
  * 第 3 步："4193 with words"（读入 "4193"；由于下一个字符不是一个数字，所以读入停止）
- *              ^
+ * ^
  * 解析得到整数 4193 。
  * 由于 "4193" 在范围 [-231, 231 - 1] 内，最终结果为 4193 。
- *
- *
+ * <p>
+ * <p>
  * 提示：
- *
+ * <p>
  * 0 <= s.length <= 200
  * s 由英文字母（大写和小写）、数字（0-9）、' '、'+'、'-' 和 '.' 组成
  */
 public class Demo8 {
 
+    public static void main(String[] args){
+        System.out.println(myAtoi("+") == 0);
+        System.out.println(myAtoi("+-12") == 0);
+        System.out.println(myAtoi("   +0 123") == 0);
+        System.out.println(myAtoi("   -42") == -42);
+        System.out.println(myAtoi("4193 with words") == 4193);
+        System.out.println(myAtoi("words and 987") == 0);
+        System.out.println(myAtoi("0 1") == 0);
+        System.out.println(myAtoi("-5-") == -5);
+        System.out.println(myAtoi("  0000000000012345678") == 12345678);
+        System.out.println(myAtoi("00000-42a1234") == 0);
+        System.out.println(myAtoi("1095502006p8") == 1095502006);
+        System.out.println(myAtoi("-1095502006p8") == -1095502006);
+        System.out.println(myAtoi("-2295502006p8") == (int) -Math.pow(2, 31));
+        System.out.println(myAtoi("    +1146905820n1") == 1146905820);
+        System.out.println(Integer.MAX_VALUE + 1);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Long.valueOf("2147483649"));
+    }
+
+    public static int myAtoi(String s){
+        StringBuilder sb = new StringBuilder();
+        char[] cl = s.toCharArray();
+        boolean containNum = false;
+        boolean canSymbol = true;
+        for(char c : cl){
+            if(c == ' '){
+                if(sb.length() > 0 || !canSymbol){
+                    break;
+                }
+                continue;
+            }else if(c == '-' || c == '+'){
+                if(!canSymbol){
+                    if(containNum){
+                        break;
+                    }
+                    return 0;
+                }
+                if(sb.length() == 0){
+                    sb.append(c);
+                    canSymbol = false;
+                }
+            }else if(c >= '0' && c <= '9'){
+                canSymbol = false;
+                if(!containNum && c == '0'){
+                    continue;
+                }
+                sb.append(c);
+                containNum = true;
+            }else{
+                break;
+            }
+        }
+
+        if(sb.length() == 0 || !containNum){
+            return 0;
+        }
+
+        if(sb.indexOf("+") == 0){
+            sb.deleteCharAt(0);
+        }
+        char[] sChar = sb.toString().toCharArray();
+        char[] minChars;
+        int min;
+        if(sb.indexOf("-") > -1){
+            // 负数
+            min = (int) -Math.pow(2, 31);
+        }else{
+            // 正数
+            min = (int) Math.pow(2, 31);
+        }
+        minChars = String.valueOf(min).toCharArray();
+        int sLen = sChar.length;
+        int mLen = minChars.length;
+        if(sLen > mLen){
+            return min;
+        }else if(sLen == mLen){
+            for(int i = 0; i < sLen; i++){
+                int a = sChar[i];
+                int b = minChars[i];
+                if(a > b)
+                    return min;
+                else if(a < b)
+                    break;
+            }
+            return Integer.parseInt(sb.toString());
+        }else{
+            return Integer.parseInt(sb.toString());
+        }
+    }
+
+    public static int myAtoi1(String s){
+        StringBuilder sb = new StringBuilder();
+        char[] cl = s.toCharArray();
+        boolean containNum = false;
+        int numIndex = 0;
+        for(char c : cl){
+            if(c == ' ' && sb.toString().length() == 0 && numIndex == 0){
+                continue;
+            }else if(c == '-' || c == '+'){
+                if(numIndex != 0 && !containNum){
+                    return 0;
+                }
+                if(sb.length() == 0){
+                    sb.append(c);
+                }else{
+                    if(containNum){
+                        break;
+                    }else{
+                        return 0;
+                    }
+                }
+            }else if(c >= '0' && c <= '9'){
+                numIndex++;
+                if(!containNum && c == '0'){
+                    continue;
+                }
+                sb.append(c);
+                containNum = true;
+            }else{
+                break;
+            }
+        }
+
+        if(sb.length() == 0 || !containNum){
+            return 0;
+        }
+        if(sb.indexOf("-") > -1 && sb.indexOf("+") > -1){
+            return 0;
+        }
+        if(sb.indexOf("+") == 0){
+            sb.deleteCharAt(0);
+        }
+        char[] sChar = sb.toString().toCharArray();
+        char[] minChars;
+        int min;
+        if(sb.indexOf("-") > -1){
+            // 负数
+            min = (int) -Math.pow(2, 31);
+        }else{
+            // 正数
+            min = (int) Math.pow(2, 31);
+        }
+        minChars = String.valueOf(min).toCharArray();
+        if(sChar.length > minChars.length){
+            return min;
+        }else if(sChar.length == minChars.length){
+            for(int i = 0; i < sChar.length; i++){
+                if(sChar[i] > minChars[i])
+                    return min;
+                else if(sChar[i] < minChars[i])
+                    break;
+            }
+            return Integer.valueOf(sb.toString());
+        }else{
+            return Integer.valueOf(sb.toString());
+        }
+    }
 }
